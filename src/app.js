f9eec66766331de5d5285f2fc1bf39d8b5ecfdeb
gilvan <g.r.almeida@live.com>
2019-04-13 19:44:23 -0300

    Creating more than one route

--- src/app.js
+++ src/app.js
@@ -4,14 +4,22 @@ import React, { PureComponent } from 'react'
 import { BrowserRouter, Link, Route } from 'react-router-dom'
 
 import About from 'components/about'
+import Contact from 'components/contact'
+import Home from 'components/home'
 
 class App extends PureComponent {
   render () {
     return (
       <BrowserRouter>
         <div>
-          <Link to='/about'>About</Link>
+          <Route path='/' exact component={Home} />
           <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+          </ul>
         </div>
       </BrowserRouter>
     )
